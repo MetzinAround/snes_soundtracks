ec1c68768825da5d8dc344d3f24fa052485462c4
Pj Metz <65838556+MetzinAround@users.noreply.github.com>
2021-04-12 16:13:52 -0400

    First Commit, no .env variables
    Waiting to get keys for .env before testing bot

---
+++ snes.js
@@ -0,0 +1,89 @@
+console.log("SNES Soundtracks booting up");
+
+//making sure npm run develop works
+if (process.env.NODE_ENV === "develop") {
+    require("dotenv").config();
+};
+
+//rules for node-schedule
+var schedule = require("node-schedule");
+var rule = new schedule.RecurrenceRule();
+  rule.dayOfWeek = 1,
+   Krule.hour = 12;
+  rule.tz = "Etc/GMT+4";
+
+//array to pull soundtracks from
+var soundtrackArray = ["https://www.youtube.com/watch?v=jV7YxfwT4r0", "https://www.youtube.com/watch?v=-QsysJwzod4", "https://youtu.be/e8tg8G2iD3U", "https://www.youtube.com/watch?v=tBWl9p0fH28&", "https://www.youtube.com/watch?v=oRxgYC5zrV4", "https://youtu.be/c68MrLSXcZ4", "https://youtu.be/85u34SUh05Y", "https://www.youtube.com/watch?v=wpchBo75N68", "https://www.youtube.com/watch?v=_4EjGXRDOH0", "https://www.youtube.com/watch?v=438nMbWncRU", "https://www.youtube.com/watch?v=UyNufyV3VCo", "https://www.youtube.com/watch?v=rJJk9Zk2h_U", "https://www.youtube.com/watch?v=A4YeAdyYq4M", "https://www.youtube.com/watch?v=_-XNMr3x0l4", "https://www.youtube.com/watch?v=sySkD2Nfwok", "https://www.youtube.com/watch?v=jUbU5tHfyCs", "https://www.youtube.com/watch?v=dIKdZ2827rM",];
+var soundtrackArrayLength = soundtrackArray.length;
+var soundtrackArrayElement = Math.floor(Math.random() * soundtrackArrayLength);
+
+//Error function listener
+function gameOver(err, reply) {
+    if (err) {
+        console.log(err.message);
+        console.log("Game Over");
+    } else {
+        console.log('Tweeted: ' + reply.text);
+    }
+}
+
+// Create an Twitter object to connect to Twitter API
+var Twit = require('twit');
+
+// Pulling keys from another file
+var config = require('./config.js');
+// Making a Twit object for connection to the API
+var T = new Twit(config);
+
+// Setting up a user stream
+var stream = T.stream('statuses/filter', { track: '@SNESSoundtracks' });
+
+// Now looking for tweet events
+// See: https://dev.twitter.com/streaming/userstreams
+stream.on('Reply to users', pressStart);
+
+
+
+
+
+// Here a tweet event is triggered!
+function pressStart(tweet) {
+
+    var id = tweet.id_str;
+    var text = tweet.text;
+    var name = tweet.user.screen_name;
+
+    const regex = /(please)/gi;
+
+    let playerOne = text.match(regex) || [];
+    let playerTwo = playerOne.length > 0;
+
+    //this helps with errors, so you can see if the regex matched and if playerTwo is true or false
+    console.log(playerOne);
+    console.log(playerTwo);
+
+
+    // checks text of tweet for mention of SNESSoundtracks
+    if ((text.includes('@SNESSoundtracks') && playerTwo === true)) {
+
+        // Start a reply back to the sender
+        var replyText = soundtrackArray[soundtrackArrayElement] + "@" + name + " Here's your soundtrack!";
+
+        // Post that tweet
+        T.post('statuses/update', { status: replyText, in_reply_to_status_id: id }, gameOver);
+
+    } else {
+        console.log("uh-uh-uh, they didn't say the magic word.");
+    }
+}
+
+function pressSelect() {
+    var replyText = soundtrackArray[soundtrackArrayElement] + "@" + name + " Here's your soundtrack!";
+    T.post('statuses/update', { status: replyText }, gameOver);
+}
+
+const job1 = schedule.scheduleJob(rule, pressSelect());
+
+job1.on("Every Day Tweet", pressSelect);
+
+
