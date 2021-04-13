acfbdcff8889a31a1649a97ce7ee03593a3aea46
Pj Metz <65838556+MetzinAround@users.noreply.github.com>
2021-04-13 11:10:09 -0400

    Code working.
    Responds to users and tweets using node schedule.

--- snes.js
+++ snes.js
@@ -9,23 +9,13 @@ if (process.env.NODE_ENV === "develop") {
 var schedule = require("node-schedule");
 var rule = new schedule.RecurrenceRule();
   rule.dayOfWeek = 1,
-   Krule.hour = 12;
+  rule.hour = 10;
   rule.tz = "Etc/GMT+4";
 
 //array to pull soundtracks from
 var soundtrackArray = ["https://www.youtube.com/watch?v=jV7YxfwT4r0", "https://www.youtube.com/watch?v=-QsysJwzod4", "https://youtu.be/e8tg8G2iD3U", "https://www.youtube.com/watch?v=tBWl9p0fH28&", "https://www.youtube.com/watch?v=oRxgYC5zrV4", "https://youtu.be/c68MrLSXcZ4", "https://youtu.be/85u34SUh05Y", "https://www.youtube.com/watch?v=wpchBo75N68", "https://www.youtube.com/watch?v=_4EjGXRDOH0", "https://www.youtube.com/watch?v=438nMbWncRU", "https://www.youtube.com/watch?v=UyNufyV3VCo", "https://www.youtube.com/watch?v=rJJk9Zk2h_U", "https://www.youtube.com/watch?v=A4YeAdyYq4M", "https://www.youtube.com/watch?v=_-XNMr3x0l4", "https://www.youtube.com/watch?v=sySkD2Nfwok", "https://www.youtube.com/watch?v=jUbU5tHfyCs", "https://www.youtube.com/watch?v=dIKdZ2827rM",];
 var soundtrackArrayLength = soundtrackArray.length;
 var soundtrackArrayElement = Math.floor(Math.random() * soundtrackArrayLength);
-
-//Error function listener
-function gameOver(err, reply) {
-    if (err) {
-        console.log(err.message);
-        console.log("Game Over");
-    } else {
-        console.log('Tweeted: ' + reply.text);
-    }
-}
 
 // Create an Twitter object to connect to Twitter API
 var Twit = require('twit');
@@ -36,14 +26,11 @@ var config = require('./config.js');
 var T = new Twit(config);
 
 // Setting up a user stream
-var stream = T.stream('statuses/filter', { track: '@SNESSoundtracks' });
+var stream = T.stream('statuses/filter', { track: '@SnesSoundtracks' });
 
 // Now looking for tweet events
 // See: https://dev.twitter.com/streaming/userstreams
-stream.on('Reply to users', pressStart);
-
-
-
+stream.on('tweet', pressStart);
 
 
 // Here a tweet event is triggered!
@@ -53,7 +40,7 @@ function pressStart(tweet) {
     var text = tweet.text;
     var name = tweet.user.screen_name;
 
-    const regex = /(please)/gi;
+    let regex = /(please)/gi;
 
     let playerOne = text.match(regex) || [];
     let playerTwo = playerOne.length > 0;
@@ -64,26 +51,48 @@ function pressStart(tweet) {
 
 
     // checks text of tweet for mention of SNESSoundtracks
-    if ((text.includes('@SNESSoundtracks') && playerTwo === true)) {
+    if (text.includes('@SnesSoundtracks') && playerTwo === true) {
 
         // Start a reply back to the sender
-        var replyText = soundtrackArray[soundtrackArrayElement] + "@" + name + " Here's your soundtrack!";
+        var replyText = ("@" + name + " Here's your soundtrack!" + soundtrackArray[soundtrackArrayElement]);
 
         // Post that tweet
         T.post('statuses/update', { status: replyText, in_reply_to_status_id: id }, gameOver);
 
     } else {
         console.log("uh-uh-uh, they didn't say the magic word.");
-    }
+    };
+
+    function gameOver(err, reply) {
+        if (err) {
+            console.log(err.message);
+            console.log("Game Over");
+        } else {
+            console.log('Tweeted: ' + reply.text);
+        }
+    };
 }
 
 function pressSelect() {
-    var replyText = soundtrackArray[soundtrackArrayElement] + "@" + name + " Here's your soundtrack!";
-    T.post('statuses/update', { status: replyText }, gameOver);
+
+    var weeklyReplyText = soundtrackArray[soundtrackArrayElement] + " Here's your soundtrack for the week!";
+    T.post('statuses/update', { status: weeklyReplyText }, gameOver2);
+
+    function gameOver2(err, reply) {
+        if (err) {
+            console.log(err.message);
+            console.log("Game Over");
+        } else {
+            console.log('Tweeted: ' + reply.text);
+        }
+    }
 }
 
-const job1 = schedule.scheduleJob(rule, pressSelect());
+ const job1 = schedule.scheduleJob(rule, pressSelect);
 
-job1.on("Every Day Tweet", pressSelect);
+ job1.on("Every Day Tweet", pressSelect);
+
+
+
 
 
