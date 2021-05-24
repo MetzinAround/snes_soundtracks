1cafaa95c307ca193a7275e7c86d5201df1a5537
Pj Metz <65838556+MetzinAround@users.noreply.github.com>
2021-05-24 13:24:04 -0400

    No more permanenet video selection.

--- snes.js
+++ snes.js
@@ -15,7 +15,7 @@ var rule = new schedule.RecurrenceRule();
 //array to pull soundtracks from
 var soundtrackArray = ["https://www.youtube.com/watch?v=jV7YxfwT4r0", "https://www.youtube.com/watch?v=-QsysJwzod4", "https://youtu.be/e8tg8G2iD3U", "https://www.youtube.com/watch?v=tBWl9p0fH28&", "https://www.youtube.com/watch?v=oRxgYC5zrV4", "https://youtu.be/c68MrLSXcZ4", "https://youtu.be/85u34SUh05Y", "https://www.youtube.com/watch?v=wpchBo75N68", "https://www.youtube.com/watch?v=_4EjGXRDOH0", "https://www.youtube.com/watch?v=438nMbWncRU", "https://www.youtube.com/watch?v=UyNufyV3VCo", "https://www.youtube.com/watch?v=rJJk9Zk2h_U", "https://www.youtube.com/watch?v=A4YeAdyYq4M", "https://www.youtube.com/watch?v=_-XNMr3x0l4", "https://www.youtube.com/watch?v=sySkD2Nfwok", "https://www.youtube.com/watch?v=jUbU5tHfyCs", "https://www.youtube.com/watch?v=dIKdZ2827rM",];
 var soundtrackArrayLength = soundtrackArray.length;
-var soundtrackArrayElement = Math.floor(Math.random() * soundtrackArrayLength);
+
 
 // Create an Twitter object to connect to Twitter API
 var Twit = require('twit');
@@ -32,9 +32,11 @@ var stream = T.stream('statuses/filter', { track: '@SnesSoundtracks' });
 // See: https://dev.twitter.com/streaming/userstreams
 stream.on('tweet', pressStart);
 
-
+console.log(soundtrackArrayLength)
 // Here a tweet event is triggered!
 function pressStart(tweet) {
+
+    let soundtrackArrayElement = Math.floor(Math.random() * soundtrackArrayLength);
 
     var id = tweet.id_str;
     var text = tweet.text;
@@ -48,13 +50,13 @@ function pressStart(tweet) {
     //this helps with errors, so you can see if the regex matched and if playerTwo is true or false
     console.log(playerOne);
     console.log(playerTwo);
-
+    
 
     // checks text of tweet for mention of SNESSoundtracks
     if (text.includes('@SnesSoundtracks') && playerTwo === true) {
 
         // Start a reply back to the sender
-        var replyText = ("@" + name + " Here's your soundtrack!" + soundtrackArray[soundtrackArrayElement]);
+        let replyText = ("@" + name + " Here you go!" + soundtrackArray[soundtrackArrayElement]);
 
         // Post that tweet
         T.post('statuses/update', { status: replyText, in_reply_to_status_id: id }, gameOver);
@@ -75,7 +77,9 @@ function pressStart(tweet) {
 
 function pressSelect() {
 
-    var weeklyReplyText = soundtrackArray[soundtrackArrayElement] + " Here's your soundtrack for the week!";
+    let soundtrackArrayElement2 = Math.floor(Math.random() * soundtrackArrayLength);
+
+    let weeklyReplyText = soundtrackArray[soundtrackArrayElement2] + " Here's your soundtrack for the week!";
     T.post('statuses/update', { status: weeklyReplyText }, gameOver2);
 
     function gameOver2(err, reply) {
